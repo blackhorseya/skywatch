a2dded1db9fce6160d64a378bce4767818f762f0
Sean Zheng <blackhorseya@gmail.com>
2023-09-30 08:25:12 +0800

    feat: add functions to encode and write data to a buffer
    - Add `encodeMessagePack` function to encode a map to MessagePack format
    - Add `writeString` function to write a string to a buffer
    - Add `writeValue` function to write different types of values to a buffer
    
    Signed-off-by: Sean Zheng <blackhorseya@gmail.com>

--- cmd/encode.go
+++ cmd/encode.go
@@ -1,6 +1,9 @@
 package cmd
 
 import (
+	"bytes"
+	"encoding/binary"
+	"encoding/json"
 	"fmt"
 
 	"github.com/spf13/cobra"
@@ -35,6 +38,68 @@ var encodeJsonCmd = &cobra.Command{
 	Run: func(cmd *cobra.Command, args []string) {
 		input := args[0]
 
-		fmt.Println("encode json called", input)
+		var data map[string]interface{}
+		err := json.Unmarshal([]byte(input), &data)
+		cobra.CheckErr(err)
+
+		encoded, err := encodeMessagePack(data)
+		cobra.CheckErr(err)
+
+		fmt.Printf("%x\n", encoded)
 	},
+}
+
+func encodeMessagePack(data map[string]interface{}) ([]byte, error) {
+	buf := new(bytes.Buffer)
+
+	// write map header
+	err := binary.Write(buf, binary.BigEndian, 0x80|byte(len(data)))
+	if err != nil {
+		return nil, err
+	}
+
+	for key, value := range data {
+		// write key
+		writeString(buf, key)
+
+		// write value
+		writeValue(buf, value)
+	}
+
+	return buf.Bytes(), nil
+}
+
+func writeString(buf *bytes.Buffer, str string) {
+	// write string length
+	binary.Write(buf, binary.BigEndian, byte(len(str)))
+
+	// write string
+	buf.WriteString(str)
+}
+
+func writeValue(buf *bytes.Buffer, value interface{}) {
+	switch v := value.(type) {
+	case int:
+		// write int value
+		binary.Write(buf, binary.BigEndian, int32(v))
+	case float64:
+		// Write float64 value
+		binary.Write(buf, binary.BigEndian, v)
+	case string:
+		// write string value
+		writeString(buf, v)
+	case bool:
+		// Write bool value
+		if v {
+			buf.Write([]byte{0xc3}) // True
+		} else {
+			buf.Write([]byte{0xc2}) // False
+		}
+	case nil:
+		// Write nil value
+		buf.Write([]byte{0xc0}) // Nil
+	default:
+		// unsupported type
+		fmt.Printf("unsupported type: %T\n", v)
+	}
 }
